46f3760792536fb49908b9bc3442b9cd27473e1a
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-09-24 12:42:42 +0200

    Tarea #20: Implementada petición simple a /health en android-clips

--- android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/MainActivity.java
+++ android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/MainActivity.java
@@ -1,16 +1,48 @@
 package com.afundacion.fp.clips;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue queue;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // Aquí lanzaremos una petición HTTP
+        this.queue = Volley.newRequestQueue(context);
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                Server.name + "/health",
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        Toast.makeText(MainActivity.this, "Hit OK", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        
+                    }
+                }
+        );
+        this.queue.add(request);
     }
 }
 
