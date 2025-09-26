d7a9574dc22cf564421b9dd54a0b61f024116b1e
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-09-26 09:48:53 +0200

    Tarea #23: Una petición de una lista

--- android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/MainActivity.java
+++ android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/MainActivity.java
@@ -1,6 +1,7 @@
 package com.afundacion.fp.clips;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -10,21 +11,28 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private RequestQueue queue;
     private Context context = this;
+    private ConstraintLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.queue = Volley.newRequestQueue(context);
+        this.mainLayout = findViewById(R.id.main_layout);
+
+        getClipList();
 
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.GET,
@@ -55,5 +63,28 @@ public class MainActivity extends AppCompatActivity {
         );
         this.queue.add(request);
     }
+
+    private void getClipList() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                Server.name + "/clips",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray) {
+                        Snackbar.make(mainLayout, "Clips received", Snackbar.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                    }
+                }
+
+        );
+        this.queue.add(jsonArrayRequest);
+    }
+
 }
 
