9e1ff381dd29a1c4a5ea66a1054e2ca7fd86b07a
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-09-30 11:57:53 +0200

    Tarea #31: Clicks en las celdas

--- android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/ClipViewHolder.java
+++ android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/ClipViewHolder.java
@@ -1,20 +1,33 @@
 package com.afundacion.fp.clips;
 
+import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ClipViewHolder extends RecyclerView.ViewHolder {
     private TextView clipTitle;
+    private Clip clip;
 
     public ClipViewHolder(@NonNull View itemView) {
         super(itemView);
         clipTitle = itemView.findViewById(R.id.text_view);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int clipId = clip.getId();
+                Context context = view.getContext();
+                Toast.makeText(context, "Touched ViewHolder with clipId: " + clipId, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     public void showData(Clip clip) {
+        this.clip = clip;
         this.clipTitle.setText(clip.getTitle());
     }
 
