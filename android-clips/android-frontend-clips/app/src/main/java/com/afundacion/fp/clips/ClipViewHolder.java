549f526ef5ca726fa8577bc615f08856a970eb78
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-09-30 13:48:01 +0200

    Tarea #32: Actividad que muestra vídeos (pero todavía no)

--- android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/ClipViewHolder.java
+++ android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/ClipViewHolder.java
@@ -1,6 +1,7 @@
 package com.afundacion.fp.clips;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,9 +22,14 @@ public class ClipViewHolder extends RecyclerView.ViewHolder {
                 int clipId = clip.getId();
                 Context context = view.getContext();
                 Toast.makeText(context, "Touched ViewHolder with clipId: " + clipId, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(context, VideoActivity.class);
+                intent.putExtra(VideoActivity.INTENT_CLIP_ID, clipId);
+                intent.putExtra(VideoActivity.INTENT_CLIP_URL, clip.getUrlVideo());
+
+                context.startActivity(intent);
             }
         });
-
     }
 
     public void showData(Clip clip) {
