64b897fa57abb66b1465f8dabc6394a80a44c630
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-10-16 10:10:48 +0200

    Tarea #42: Petición POST de registro

--- android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/RegisterActivity.java
+++ android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/RegisterActivity.java
@@ -9,11 +9,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RegisterActivity extends AppCompatActivity {
     private EditText editTextUser;
     private EditText editTextPassword;
     private Button buttonRegister;
     private Context context = this;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +39,43 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Usuario: " + editTextUser.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                sendPostRequest();
             }
         });
 
+        requestQueue = Volley.newRequestQueue(this);
+
 
     }
+
+    private void sendPostRequest(){
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("username", editTextUser.getText().toString());
+            requestBody.put("password", editTextPassword.getText().toString());
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                Server.name + "/users",
+                requestBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(context, "Cuenta creada con éxito", Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        this.requestQueue.add(request);
+    }
+
 }
