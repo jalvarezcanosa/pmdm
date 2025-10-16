f68abd6592aa017fd1ecc98862e87a2f2d0eb1e3
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-10-16 09:34:16 +0200

    Tarea #41: ¿Qué escribió el usuario?

--- android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/RegisterActivity.java
+++ android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/RegisterActivity.java
@@ -1,13 +1,36 @@
 package com.afundacion.fp.sessions;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText editTextUser;
+    private EditText editTextPassword;
+    private Button buttonRegister;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editTextUser = findViewById(R.id.edit_text_1);
+        editTextPassword = findViewById(R.id.edit_text_2);
+        buttonRegister = findViewById(R.id.buttoncrear_usuario);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Usuario: " + editTextUser.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
