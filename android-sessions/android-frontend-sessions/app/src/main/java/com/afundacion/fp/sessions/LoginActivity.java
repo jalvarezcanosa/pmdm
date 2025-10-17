acd31a6d73b64604fdfff8d606c85025c23bc5dc
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-10-17 13:04:30 +0200

    Tarea #45: Petición POST de inicio de sesión

--- android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/LoginActivity.java
+++ android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/LoginActivity.java
@@ -1,14 +1,32 @@
 package com.afundacion.fp.sessions;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
     private Button buttonNoAccount;
+    private Button buttonLogin;
+    private EditText username;
+    private EditText password;
+    private RequestQueue requestQueue;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +34,11 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         buttonNoAccount = findViewById(R.id.button_no_cuenta);
+        buttonLogin = findViewById(R.id.button_login);
+        username = findViewById(R.id.edit_text_user);
+        password = findViewById(R.id.edit_text_password);
+
+        requestQueue = Volley.newRequestQueue(this);
 
         buttonNoAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,8 +47,48 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendPostLogin();
+            }
+        });
     }
 
+    private void sendPostLogin() {
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("username", username.getText().toString());
+            requestBody.put("password", password.getText().toString());
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
 
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                Server.name + "/sessions",
+                requestBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String receivedToken;
+                        try {
+                            receivedToken = response.getString("sessionToken");
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
 
+                        Toast.makeText(context, "Token " + receivedToken, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        this.requestQueue.add(request);
+    }
 }
