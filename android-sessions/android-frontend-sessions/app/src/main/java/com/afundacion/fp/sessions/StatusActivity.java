8cd79815d323e5bc8bd46f8aaef278b089778acc
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-10-21 09:27:47 +0200

    Tarea #50: ¿Es un pájaro? ¿Es un avión? ¡Es un botón!

--- android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/StatusActivity.java
+++ android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/StatusActivity.java
@@ -3,9 +3,11 @@ package com.afundacion.fp.sessions;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,6 +19,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,12 +28,24 @@ public class StatusActivity extends AppCompatActivity {
     private Context context = this;
     private RequestQueue queue;
     private TextView textViewStatus;
+    FloatingActionButton buttonPutStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
         textViewStatus = findViewById(R.id.text_status);
+        buttonPutStatus = findViewById(R.id.button_open_dialog);
+        buttonPutStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(context);
+                myBuilder.setPositiveButton("Actualizar estado", null); // Esto añade un botón al diálogo
+                AlertDialog myDialog = myBuilder.create(); // Esta línea es como 'new AlertDialog'
+                myDialog.show();
+
+            }
+        });
         // Inicializamos la cola de peticiones y preparamos la petición inicial
         queue = Volley.newRequestQueue(this);
         getUserStatus();
