46869ec8aedadcb356a5392975e465dda01b3d51
canosa02 <jorge220604@gmail.com>
2025-11-23 15:31:06 +0100

    Creación del nodo ghost para reutilización del modo Mob

--- godot/las-aventuras-de-ricardo-y-federico/scripts/mob.gd
+++ godot/las-aventuras-de-ricardo-y-federico/scripts/mob.gd
@@ -11,7 +11,6 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 100.0
-	
 
 	move_and_slide()
 
